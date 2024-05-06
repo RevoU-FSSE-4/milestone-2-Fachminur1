d4f789eeb963839e0d2b7546c01bdfeae557ac4a
Fachminur1 <fachminur2836@gmail.com>
2024-05-06 21:24:25 +0700

    add route and form event

--- reacttypescript/src/App.tsx
+++ reacttypescript/src/App.tsx
@@ -1,23 +1,33 @@
 import React, { useEffect, useState } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Menu from './Menu/Menu';
 import GridNews from './Grid/GridNews';
 import { NewsArticle } from './items/items';
 import './App.css'; // Import the CSS file
 
-function App() {
+function App(): JSX.Element {
   const [items, setItems] = useState<NewsArticle[]>([]);
   const [active, setActive] = useState<number>(1);
   const [category, setCategory] = useState<string>('general');
   const [loading, setLoading] = useState<boolean>(false);
   const [error, setError] = useState<string>('');
+  const [searchQuery, setSearchQuery] = useState<string>('');
 
   useEffect(() => {
     setLoading(true);
     setError('');
 
-    fetch(
-      `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=730cb6248dba456a9740f8c152836660`
-    )
+    let apiUrl = `https://newsapi.org/v2/top-headlines?country=us&apiKey=730cb6248dba456a9740f8c152836660`;
+
+    if (category !== 'general') {
+      apiUrl += `&category=${category}`;
+    }
+
+    if (searchQuery) {
+      apiUrl += `&q=${encodeURIComponent(searchQuery)}`;
+    }
+
+    fetch(apiUrl)
       .then((res) => {
         if (!res.ok) {
           throw new Error('Failed to fetch data');
@@ -33,20 +43,41 @@ function App() {
       .finally(() => {
         setLoading(false);
       });
-  }, [category]);
+  }, [category, searchQuery]);
+
+  const handleSearchSubmit = (e: React.FormEvent<HTMLFormElement>): void => {
+    e.preventDefault();
+  };
 
   return (
-    <div className="App">
-      <h1 className="title">See The Latest News</h1>
-      <Menu active={active} setActive={setActive} setCategory={setCategory} />
-      {loading ? (
-        <p>Loading...</p>
-      ) : error ? (
-        <p>Error: {error}</p>
-      ) : (
-        <GridNews items={items} />
-      )}
-    </div>
+    <Router>
+      <div className="App">
+        <h1 className="title">See The Latest News</h1>
+        <Menu active={active} setActive={setActive} setCategory={setCategory} />
+        {/* Search Form */}
+        <form onSubmit={handleSearchSubmit}>
+          <input
+            type="text"
+            value={searchQuery}
+            onChange={(e) => setSearchQuery(e.target.value)}
+            placeholder="Search for news..."
+          />
+          <button type="submit">Search</button>
+        </form>
+        <Routes>
+          <Route path="/" element={
+            loading ? (
+              <p>Loading...</p>
+            ) : error ? (
+              <p>Error: {error}</p>
+            ) : (
+              <GridNews items={items} />
+            )
+          } />
+          {}
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
